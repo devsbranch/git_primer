e37ef51721997bae9d578daa61dbc0742bd50424
Alison Mukoma __sonlinux__ <alison.mukoma@trivago.com>
2020-11-10 19:19:55 +0100

    optimised function result time

---
+++ git_intro.py
@@ -0,0 +1,18 @@
+# GIT AND GITHUB
+
+# Which changes were made
+# who made the changes # Patricia
+# when were the changes made # timestamp
+# why were the changes made
+
+# git - tool VCS
+# /source_folder
+# tell git that it should start tracking this folder
+# folder -> repository
+
+def add_numbers():
+    return 3 + 5  # 0.0004 ms 0.0002 ms
+
+# tell git to start tracking folder
+# git - service
+# git  init # tell track your folder
